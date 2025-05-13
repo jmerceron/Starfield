76731f270dddcc03832628c9ac092eb003fc2ae7
jmerceron <julien.merceron@gmail.com>
2025-05-13 17:43:53 +0900

    Add first batch of implementation files

--- SDL_Controller.cpp
+++ SDL_Controller.cpp
@@ -1,1 +1,22 @@
-# Content of SDL_Controller.cpp
+#include "SDL_Controller.h"
+#include <iostream>
+
+SDL_GameController* open_controller(int joystick_index) {
+    if (SDL_IsGameController(joystick_index)) {
+        SDL_GameController* controller = SDL_GameControllerOpen(joystick_index);
+        if (controller) {
+            std::cout << "Found a compatible controller: " << SDL_GameControllerName(controller) << std::endl;
+            return controller;
+        }
+        else {
+            std::cerr << "Could not open gamecontroller " << joystick_index << ": " << SDL_GetError() << std::endl;
+        }
+    }
+    return nullptr;
+}
+
+void close_controller(SDL_GameController* controller) {
+    if (controller) {
+        SDL_GameControllerClose(controller);
+    }
+}
--- conversion.cpp
+++ conversion.cpp
@@ -1,1 +1,11 @@
-# Content of conversion.cpp
+#include "conversion.h"
+#include <stdio.h>
+
+namespace Julien_Conversion {
+
+    void convert_to_string(float value, char* buffer)
+    {
+        sprintf_s(buffer, 10, "%.2f", value);
+    }
+
+} // namespace Julien_Conversion
